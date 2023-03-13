9b6f8b6fe33d372bdb7cfe9789e3a770b21b55eb
cacaprog <46378837+cacaprog@users.noreply.github.com>
2023-03-13 11:29:57 -0300

    upload the program to review and make it run correctly

---
+++ 01_hello/hello.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/env python3
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
