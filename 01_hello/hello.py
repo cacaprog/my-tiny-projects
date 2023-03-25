55dd6c186f424c566c6665eb7a91105f3ea61641
cacaprog <cacaprog@gmail.com>
2023-03-25 14:59:18 -0300

    updated file created by new.py

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,18 +1,33 @@
 #!/home/cacaprog/anaconda3/bin/python
+"""
+Author : cacaprog <cacaprog@localhost>
+Date   : 2023-03-25
+Purpose: Test to create a new program
+"""
 
 import argparse
 
 
 # --------------------------------------------------
 def get_args():
+    """Get command-line arguments"""
 
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', default='World', help='Name to greet')
+    parser = argparse.ArgumentParser(
+        description='Python test',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-n',
+                        '--name',
+                        help='Name to greet',
+                        metavar='name',
+                        type=str,
+                        default='World')
+
     return parser.parse_args()
-
 
 # --------------------------------------------------
 def main():
+    """Make a jazz noise here"""
 
     args = get_args()
     print('Hello, ' + args.name + '!')
