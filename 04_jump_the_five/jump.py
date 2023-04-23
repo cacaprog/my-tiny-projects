a469f5492033589da9f42165bf1c776314aee32e
cacaprog <cacaprog@gmail.com>
2023-04-23 12:22:50 -0300

    five solutions tested

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -31,11 +31,30 @@ def main():
     args = get_args()
     
     jumper = {'1':'9', '2':'8', '3':'7', '4':'6', '5':'0', '6':'4', '7':'3', '8':'2', '9':'1', '0':'5'}     
+# solution 1
+ #   for char in args.text:
+ #       print(jumper.get(char, char), end='')
+ #   print() # print a new line after done
 
-    for char in args.text:
-        print(jumper.get(char, char), end='')
-    print() # print a new line after done
-      
+# solution 2
+ #   new_text = ''
+#   for char in args.text:
+#        new_text += jumper.get(char, char)
+#    print(new_text)
+
+# solution 3
+#    new_text = []
+#    for char in args.text:
+#        new_text.append(jumper.get(char, char))
+#    print(''.join(new_text))
+
+# solution 4
+#    print(''.join([jumper.get(char, char) for char in args.text]))
+
+
+# solution 5
+    print(args.text.translate(str.maketrans(jumper)))
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
